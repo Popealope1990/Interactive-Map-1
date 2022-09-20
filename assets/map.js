0f155bf2a165e643404c800dc7c377c626e4d34b
Benjamin Pope <buddypope1234@gmail.com>
2022-09-19 19:43:10 -0500

    Used leaflet documentation to get map to load my current position

--- assets/map.js
+++ assets/map.js
@@ -1,1 +1,16 @@
-console.log('Hello from Map.js')
+// console.log('Hello from Map.js')
+
+// Initialize map here
+var map = L.map('map').fitWorld();
+
+// Map tile layer
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// setting map to your location
+
+map.locate({setView: true, maxZoom: 16});
+
